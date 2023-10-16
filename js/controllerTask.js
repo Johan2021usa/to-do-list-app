5d34b80d24624fe6875d2f8c809f37f7cdf08193
johanLasso <lassojairo@gmail.com>
2023-10-15 19:39:32 -0500

    V.1.1 Visual bugs fixed, no persistence

--- js/controllerTask.js
+++ js/controllerTask.js
@@ -2,23 +2,23 @@ import {createTask, cleanForm, modifyStatus, deleteTask} from "./viewTask.js";
 import {Task} from "./modelTask.js";
 
 ( ()=>{
-        const form = document.querySelector("[data-button-form]");
-        const taskContainer = document.querySelector("[data-cont-task]");
+    const form = document.querySelector("[data-button-form]");
+    const taskContainer = document.querySelector("[data-cont-task]");
 
-        const createActions = (event) => {
-            event.preventDefault();
-            const inputFormText = document.querySelector("[data-form-inp]").value;
-            let task = new Task(inputFormText.toString(), "pending");
-            createTask(task);
-            cleanForm();
-        }
-
-        const taskContainerActions = (event) => {
-            modifyStatus(event.target, event.target.value);
-            deleteTask(event.target);
-        }
-        form.addEventListener("submit", createActions);
-        taskContainer.addEventListener("click", taskContainerActions)
+    const createActions = (event) => {
+        event.preventDefault();
+        const inputFormText = document.querySelector("[data-form-inp]").value;
+        let task = new Task(inputFormText.toString(), "pending");
+        createTask(task);
+        cleanForm();
     }
-) ()
+
+    const taskContainerActions = (event) => {
+        modifyStatus(event.target, event.target.value);
+        deleteTask(event.target);
+    }
+    form.addEventListener("submit", createActions);
+    taskContainer.addEventListener("click", taskContainerActions)
+    }
+)()
 
--- js/viewTask.js
+++ js/viewTask.js
@@ -7,6 +7,7 @@ const createTask = (task)=>{
             <select class="task-container__status _status--pending" data-type-status>
                 <option class="task-container__option-pending" value="pending">pending</option>
                 <option class="task-container__option-completed" value="completed">completed</option>
+                <option class="task-container__option-ongoing" value="ongoing">ongoing</option>
             </select>
         </li>
         <li><button class="task-container__delete">delete</button></li>
@@ -18,7 +19,7 @@ const createTask = (task)=>{
 };
 
 const cleanForm = ()=>{
-    //reset method only works with forms
+    //reset method only works on forms
     document.querySelector("[data-button-form]").reset();
 }
 
@@ -26,16 +27,22 @@ const modifyStatus = (element, status)=>{
     const ulElement = (element.parentElement).parentNode;
 
     if(status === "pending" && ulElement.classList.contains("task-container__u-list")){
-        ulElement.classList.remove("_u-list_completed");
-        element.classList.remove("_status--completed");
+        ulElement.classList.remove("_u-list_completed", "_u-list_ongoing");
+        element.classList.remove("_status--completed", "_status--ongoing");
         ulElement.classList.add("_u-list_pending");
         element.classList.add("_status--pending");
     }
     if(status === "completed" && ulElement.classList.contains("task-container__u-list")){
-        ulElement.classList.remove("_u-list_pending");
-        element.classList.remove("_status--pending");
+        ulElement.classList.remove("_u-list_pending", "_u-list_ongoing");
+        element.classList.remove("_status--pending", "_status--ongoing");
         ulElement.classList.add("_u-list_completed");
         element.classList.add("_status--completed");
+    }
+    if(status === "ongoing" && ulElement.classList.contains("task-container__u-list")){
+        ulElement.classList.remove("_u-list_pending", "_u-list_completed");
+        element.classList.remove("_status--pending", "_status--completed");
+        ulElement.classList.add("_u-list_ongoing");
+        element.classList.add("_status--ongoing");
     }
 }
 
